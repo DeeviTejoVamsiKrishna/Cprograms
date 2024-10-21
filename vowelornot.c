1b43533fbfa8dfa115dd1ac78d20c9bb64bec7bd
DeeviTejoVamsiKrishna <deevivamsi@gmail.com>
2024-10-21 11:57:42 +0530

    Create vowelornot.c

---
+++ vowelornot.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+char ch;
+printf("enter the char");
+scanf("%c",&ch);
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='u')
+{
+    printf("vowel");
+}
+else
+{
+     printf("not vowel");
+}
+}
