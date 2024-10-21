67595e17d0721dd6d092b94d8c4e78adef403bc8
DeeviTejoVamsiKrishna <deevivamsi@gmail.com>
2024-10-21 11:39:43 +0530

    Create eligiblityofvote.c

---
+++ eligiblityofvote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+void main()
+{
+    int a;
+    printf("enter a");
+    scanf("%d",&a);
+    if(a>18)
+    {
+        printf("eligible for vote");
+    }
+    else
+    {
+        printf("not eligible for vote");
+    }
+}
