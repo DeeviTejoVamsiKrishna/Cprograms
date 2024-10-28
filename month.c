c1453fb99aa9c4ccd779817356557fa0f7426e2b
DeeviTejoVamsiKrishna <deevivamsi@gmail.com>
2024-10-28 12:30:57 +0530

    Create month.c

---
+++ month.c
@@ -0,0 +1,52 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+void main()
+{
+    int a;
+    printf("enter a value");
+    scanf("%d",a);
+    switch(a);
+    {
+        case 1:
+        printf("january");
+        break;
+        casen 2:
+        printf("february");
+        break:
+        case 3:
+        printf("march");
+        break;
+         case 4:
+        printf("april");
+        break;
+       case 5:
+        printf("may");
+        break;
+        case 6:
+        printf("june");
+        break;
+        
+        case 1:
+        printf("july");
+        break;
+        
+        case 1:
+        printf("auguest");
+        break;
+        
+        case 1:
+        printf("september");
+        break;
+        
+        case 1:
+        printf("october");
+        break;
+        case 1:
+        printf("november");
+        break;
+        case 1:
+        printf("december");
+        break;
+        
+    }
+}
