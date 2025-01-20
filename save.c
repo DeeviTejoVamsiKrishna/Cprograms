9abe3842021635c1e1e8bc266e21ec103411e7fa
DeeviTejoVamsiKrishna <deevivamsi@gmail.com>
2025-01-20 12:29:04 +0530

    Create save.c

---
+++ save.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+    void main(){
+  char str1[10],str2[5];
+  int l,m;
+  printf("enter str1,str2");
+scanf("%s",str1);
+l=strlen(str1);
+scanf("%s",str2);
+m=strlen(str2);
+strcmp(str1,str2);
+if(l==m)
+{
+printf("%d",equal);
+}
+else
+printf("%d",notequal);
+}
